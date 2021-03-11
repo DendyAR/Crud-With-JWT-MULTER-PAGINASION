c10714764293c9eaaa1035d1e8c858ae85378b51
Dendy Anugerah Ramadhan <49724910+DendyAR@users.noreply.github.com>
2021-03-12 02:36:54 +0700

    Add files via upload

---
+++ route/Chat.js
@@ -0,0 +1,13 @@
+// const route=require('express').Router()
+// const chatController = require('../controller/Chat')
+// // const searchController = require('../controller/Search')
+
+// route.get('/', chatController.getAlluser)
+// route.post('/', chatController.addNewuser)
+// // route.get('/:id_name', chatController.getUser)
+// // route.patch('/:username', chatController.updateUser)
+// // route.get('/:username', searchController.sortByname)
+// // route.delete('/:name', chatController.deleteUser)
+// // route.get('/chat', chatController.getAlluser)
+
+// module.exports=route
